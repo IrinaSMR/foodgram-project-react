95489ab5056996732573f7f1add8f6152f221ba9
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-07 23:52:39 +0300

    Revert "d-c chfnges"
    This reverts commit c740486ae0a214abe28e7dfc920f4e5cf02f8409.

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -13,6 +13,9 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
+    path('recipes/download_shopping_cart/<int:id>',
+         download_shopping_cart,
+         name='download_shopping_cart'),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
     path('', include(router.urls)),
