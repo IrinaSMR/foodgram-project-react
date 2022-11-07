1177958de4e5625593d0060acbe8b4e7a9068f57
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-07 23:23:23 +0300

    url changes

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -13,11 +13,11 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path(
         'recipes/download_shopping_cart/',
         download_shopping_cart(),
         name='download_shopping_cart'),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('', include('djoser.urls')),
-    path('', include(router.urls)),
 ]
