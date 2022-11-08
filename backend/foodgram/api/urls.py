b5cb35d7579be09c1d93176f94b598ffe35d9590
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-08 16:46:20 +0300

    Revert "urls problems"
    This reverts commit 0c6499f3e8bf2a264be6ff2561091b13a2b231cd.

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet
-
+from .views import (IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet,
+                    download_shopping_cart)
 
 app_name = 'api'
 
@@ -13,6 +13,10 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('recipes/download_shopping_cart/<int:id>',
+         download_shopping_cart,
+         name='download_shopping_cart'),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('', include(router.urls)),
 ]
