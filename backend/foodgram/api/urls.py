f51ca45390db6dac47096827d45dae1178916ad3
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-09 18:38:15 +0300

    changed api urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,12 +3,14 @@ from rest_framework import routers
 
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet
 
+app_name = 'api'
+
 router = routers.DefaultRouter()
 
-router.register('users', UsersViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
-router.register('tags', TagViewSet)
+router.register('users', UsersViewSet, basename='users')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('', include(router.urls)),
