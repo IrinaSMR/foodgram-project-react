9bf4670bb4f09c276f8e3165107d25bf6dca5c38
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-12 18:54:34 +0300

    after 2d review

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'localhost', '84.201.156.75']
 
