97f6244d0b5e3e64f09460c69e7d8ca0d8762449
Ирина Смирнова <ismirnova83@yandex.ru>
2022-11-13 18:35:25 +0300

    urls to dshcart

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,19 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet
+from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
+                    UsersViewSet, download_shopping_cart)
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 
-router.register('users', UsersViewSet, basename='users')
-router.register('ingredients', IngredientViewSet, basename='ingredients')
-router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('tags', TagViewSet, basename='tags')
+router.register(r'users', UsersViewSet, basename='users')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-    # path('recipes/download_shopping_cart/', download_shopping_cart),
+    path('recipes/download_shopping_cart/', download_shopping_cart),
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
